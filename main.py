7ec660f0fb1a72c19c050021ab88a3d4db11a263
Eric Harmeling <eric.harmeling@cockroachlabs.com>
2021-10-08 14:19:42 -0400

    main.py now parses postgres url prefix

--- main.py
+++ main.py
@@ -1,13 +1,15 @@
 from sqlalchemy import create_engine
+import urllib
 import os
+
 
 def connect(db_uri):
     engine = create_engine(db_uri)
     engine.connect()
     print('Hey! You successfully connected to your CockroachDB cluster.')
 
-if __name__ == '__main__':
 
+if __name__ == '__main__':
     conn_string = input('Enter your node\'s connection string:\n')
     # For cockroach demo:
     # cockroachdb://demo:<demo_password>@127.0.0.1:26257/defaultdb?sslmode=require
@@ -15,7 +17,12 @@ if __name__ == '__main__':
     # cockroachdb://<username>:<password>@<globalhost>:26257/<cluster_name>.defaultdb?sslmode=verify-full&sslrootcert=<certs_dir>/<ca.crt>
 
     try:
-        connect(os.path.expandvars(conn_string))
+        db_uri = os.path.expandvars(conn_string)
+        db_uri = urllib.parse.unquote(db_uri)
+        psycopg_uri = db_uri.replace(
+            'postgresql://', 'cockroachdb://').replace(
+                'postgres://', 'cockroachdb://')
+        connect(psycopg_uri)
     except Exception as e:
         print('Failed to connect to database.')
         print('{0}'.format(e))
